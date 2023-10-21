beeedb548c9854d21b322b66dddb6bc4e381c48e
dvesele <karyagina.paul@gmail.com>
2023-10-21 17:26:59 +0500

    Initial commit

---
+++ src/main/java/com/simpleenglish/Simple_Engl_bot/SimpleEnglBotApplication.java
@@ -0,0 +1,13 @@
+package com.simpleenglish.Simple_Engl_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEnglBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEnglBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpleenglish/Simple_Engl_bot/SimpleEnglBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleenglish.Simple_Engl_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEnglBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
